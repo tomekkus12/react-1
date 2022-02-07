7ae629aef62d5faf69973f5d99551f31a74a466b
Tomasz Kusmierczyk <tomasz.t.kusmierczyk@gmail.com>
2022-02-07 17:16:48 +0100

    Step poprawiony

--- src/Step.js
+++ src/Step.js
@@ -9,24 +9,23 @@ class Step extends Component {
         this.state = {realValue: this.props.realStep};
     }
 
-    update = () => {
-        this.setState({realValue: parseInt(document.getElementById('step-value').value)});
-        this.props.stepMethod((this.state.realValue));
-    }
 
-    handleChange(event) {
+    handleChange = (event) => {
         
         this.props.stepMethod((this.state.realValue));
         this.setState({realValue: parseInt(event.target.value)});
 
       }
 
+
+
+
     render() {
 
         return (
             <div className="step">
                 Krok:
-                <input type="number" id="step-value" min="0" value={this.state.realValue} onClick={this.handleChange.bind(this)} onChange={this.handleChange.bind(this)} onBlur={this.handleChange.bind(this)}/>
+                <input type="number" id="step-value" min="0" value={this.state.realValue} onClick={this.handleChange} onChange={this.handleChange} onBlur={this.handleChange}/>
             </div>
         );
     }
